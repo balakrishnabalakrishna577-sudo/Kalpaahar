466313461e6a1d10b433a1c3d439ba4ad714048c
Balakrishna <balakrishnabalakrishna577@gmail.com>
2026-08-29 11:23:20 +0530

    Auto-create superuser on Render deploy

--- build.sh
+++ build.sh
@@ -10,3 +10,17 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py seed_ebooks
+
+# Create superuser automatically if it does not already exist
+python manage.py shell -c "
+from accounts.models import User
+if not User.objects.filter(email='admin@kalpaahar.in').exists():
+    User.objects.create_superuser(
+        email='admin@kalpaahar.in',
+        password='Kalpaahar@Admin2026',
+        name='Admin'
+    )
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
